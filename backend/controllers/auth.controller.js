41bf6ac4832e7f7100fbb560194beafcc6fcedc5
OOOyCode <epice8303@gmail.com>
2026-04-24 14:56:01 +0200

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -169,7 +169,7 @@ export const googleLogin = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // FIXED
+      secure: true, // FIXED
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
