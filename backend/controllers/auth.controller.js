734c9b203803415b1897e5f4c709c17f4df39d91
OOOyCode <epice8303@gmail.com>
2026-04-24 15:10:49 +0200

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -97,7 +97,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -170,7 +170,7 @@ export const googleLogin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true, // FIXED
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
