db4bc724d9b566a82113c2c4adf644d0c3cd9153
OOOyCode <epice8303@gmail.com>
2026-04-30 00:25:02 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
@@ -9,11 +11,50 @@ import Register from "./pages/Register";
 import ManagePosts from "./pages/ManagePosts";
 import DrawingPage from "./pages/HandDrawing";
 import NotFound from "./pages/NotFound";
+import { api } from "./api/axios";
+import NavbarMobile from "./components/Navbar/NavbarMobile";
 
 function App() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await api.get("/auth/me");
+        setUser(res.data);
+      } catch (err) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout");
+      setUser(null);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
       <Navbar />
+      <NavbarMobile
+        user={user}
+        menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}
+        navigate={navigate}
+        handleLogout={handleLogout}
+      />
 
       <Routes>
         <Route path="/" element={<Home />} />
